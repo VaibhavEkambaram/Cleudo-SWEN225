6169926cc0428ef53edaf05800b0cc574a24ca0a
vaibhav265 <50600483+vaibhav265@users.noreply.github.com>
2020-08-19 16:02:18 +1200

    started menu bar

--- Implementation/src/View/Table.java
+++ Implementation/src/View/Table.java
@@ -13,9 +13,36 @@ public class Table extends Observable {
     public Table() {
         JFrame gameFrame = new JFrame("Cluedo");
         gameFrame.setLayout(new BorderLayout());
+        final JMenuBar tableMenuBar = createTableMenuBar();
+        gameFrame.setJMenuBar(tableMenuBar);
         gameFrame.setSize(OUTER_FRAME_DIMENSION);
-
         gameFrame.setVisible(true);
+    }
 
+
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
+        tableMenuBar.add(createFileMenu());
+        tableMenuBar.add(createGameMenu());
+        return tableMenuBar;
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+
+        final JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(e -> System.exit(0));
+        fileMenu.add(exitMenuItem);
+        return fileMenu;
+    }
+
+    private JMenu createGameMenu() {
+        final JMenu gameMenu = new JMenu("Game");
+        final JMenuItem setupGameMenuItem = new JMenuItem("Setup Game");
+        setupGameMenuItem.addActionListener(e -> {
+
+        });
+        gameMenu.add(setupGameMenuItem);
+        return gameMenu;
     }
 }
