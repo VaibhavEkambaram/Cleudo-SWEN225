7670410aefcc4615abe60160631ec4b7bf604d6c
vaibhav265 <50600483+vaibhav265@users.noreply.github.com>
2020-08-22 12:16:44 +1200

    added non-functional gui for player setup

--- Implementation/src/View/SetupPlayers.java
+++ Implementation/src/View/SetupPlayers.java
@@ -2,26 +2,43 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SetupPlayers {
-    public void setPlayers(String[] characterNames){
-        for(int i=0; i < 6; i++) {
-            JPanel fields = new JPanel(new GridLayout(3, 1));
-            JLabel label = new JLabel("Enter your name then select your player token");
+    public void setPlayers(String[] characterNames) {
+        ArrayList<String> charNames = new ArrayList<>();
+        ArrayList<String> used = new ArrayList<>();
+
+        charNames.addAll(Arrays.asList(characterNames));
+
+        for (int i = 0; i < 6; i++) {
+            JPanel fields = new JPanel(new GridLayout(5, 2));
+            JLabel label = new JLabel("Enter your name then select your player token ");
             JTextField nameField = new JTextField();
+            nameField.setText("Player " + (i + 1));
 
             ButtonGroup b = new ButtonGroup();
-            JRadioButton jRadioButton1 = new JRadioButton();
-            b.add(jRadioButton1);
-
-            JRadioButton jRadioButton2 = new JRadioButton();
-            b.add(jRadioButton2);
-            fields.add(jRadioButton1);
-            fields.add(jRadioButton2);
 
             fields.add(label);
             fields.add(nameField);
-            JOptionPane.showMessageDialog(null, fields, "Set Player Preferences", JOptionPane.DEFAULT_OPTION);
+
+            for (String character : charNames) {
+                JRadioButton jRadioButton1 = new JRadioButton();
+                if (used.contains(character)) {
+                    jRadioButton1.setEnabled(false);
+                }
+
+                jRadioButton1.setText(character);
+                jRadioButton1.setActionCommand(character);
+                b.add(jRadioButton1);
+                fields.add(jRadioButton1);
+            }
+
+            JOptionPane.showMessageDialog(null, fields, "Set Player Preferences", JOptionPane.OK_OPTION);
+            System.out.println(b.getSelection().getActionCommand());
+            used.add(b.getSelection().getActionCommand());
         }
     }
 }
