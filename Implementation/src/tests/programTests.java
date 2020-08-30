5dd481791257f71e97e81ac29bdc218c65fb1ab6
Baxter Kirikiri <baxter.kirikiri@gmail.com>
2020-08-30 20:48:47 +1200

    Added correct accusation test and made it so the program exits after a win

--- Implementation/src/tests/programTests.java
+++ Implementation/src/tests/programTests.java
@@ -168,7 +168,7 @@ public class programTests {
     }
 
     /**
-     * Tests if the player can no accuse after making a false accusation
+     * Tests if the player can no longer accuse after making a false accusation
      *
      * @author Baxter Kirikiri
      */
@@ -179,11 +179,26 @@ public class programTests {
         game.actionTransition();
         game.setMurderScenario(new Scenario((WeaponCard)game.getWeaponCardsMap().get("Dagger"), (RoomCard)game.getRoomCardsMap().get("Conservatory"), (CharacterCard)game.getCharacterCardsMap().get("Col. Mustard")));
 
-
         assertEquals(true, game.getCurrentPlayer().getCanAccuse());
 
         game.makeAccusation(game.getCurrentPlayer());
 
         assertEquals(false, game.getCurrentPlayer().getCanAccuse());
+    }
+
+    /**
+     * Tests if the player wins the game after a correct accusation
+     *
+     * @author Baxter Kirikiri
+     */
+    @Test
+    public void checkCorrectAccusation(){
+        Game game = new Game(false);
+        game.setMovesRemaining(2);
+        game.actionTransition();
+        game.setMurderScenario(new Scenario((WeaponCard)game.getWeaponCardsMap().get("Candlestick"), (RoomCard)game.getRoomCardsMap().get("Library"), (CharacterCard)game.getCharacterCardsMap().get("Miss Scarlett")));
+        game.finishTransition();
+
+        assertEquals(Game.States.FINISHED, game.getGameState());
     }
 }
